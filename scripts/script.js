698cb7862143b71aa9261a3f7e7d1f8da5528dba
Lucas <viper20@poczta.fm>
2017-08-22 21:16:59 +0200

    save

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,43 @@
-$(function(){
+$(document).ready(function(){
     getQuote();
-    $('.trigger').on('click', function() {
+    $('.trigger').click(function() {
         getQuote();
-    });
+    })
 });
 
-    var prefix = "https://cors-anywhere.herokuapp.com/",
-        tweetLink = "https://twitter.com/intent/tweet?text=",
-        quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var prefix = "https://cors-anywhere.herokuapp.com/",
+    tweetLink = "https://twitter.com/intent/tweet?text=",
+    quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
-    function getQuote() {
-        $.getJSON(quoteUrl, createTweet);
-        $.getJSON(prefix + quoteUrl, createTweet);
-        $.ajaxSetup({ cache: false });
-    }
+       
+function getQuote() {
+    $.getJSON(prefix + quoteUrl, createTweet);
+    $.ajaxSetup({ cache: false });
+}
 
-    function createTweet (input) {
-        var data = input[0],
-            quoteText = $(data.content).text().trim(),
-            quoteAuthor = data.title;
+function createTweet (input) {
+    var data = input[0],
+        quoteText = $(data.content).text().trim(),
+        quoteAuthor = data.title;
     
-        if(!quoteAuthor.length) {
-            quoteAuthor = 'Unknown author';
-        }
-
-        var tweetText = 'Quote of the day - ' + quoteText + ' Author: ' + quoteAuthor;
-
-        if(tweetText.length > 140) {
-            getQuote();
-        } else {
-            var tweet = tweetLink + encodeURIComponent(tweetText);
-            $('.quote').text(quoteText);
-            $('.author').text('Author: ' + quoteAuthor);
-            $('.tweet').attr('href', tweet);
-        }
+    if(!quoteAuthor.length) {
+        quoteAuthor = 'Unknown author';
     }
-    var $quote = $('.quote'),
-        $author = $('.author');
-    $quote.css('color', 'white').css('font-style', 'italic');
-    $author.css('color', 'white').css('font-size', '32px');
+
+    var tweetText = 'Quote of the day - ' + quoteText + ' Author: ' + quoteAuthor;
+
+    if(tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $('.quote').text(quoteText);
+        $('.author').text('Author: ' + quoteAuthor);
+        $('.tweet').attr('href', tweet);
+    }
+}
+
+var $quote = $('.quote'),
+    $author = $('.author');
+
+$quote.css('color', 'white').css('font-style', 'italic');
+$author.css('color', 'white').css('font-size', '32px');
